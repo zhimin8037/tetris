ca831b4cb9788203634276f96f8179fcd80cc627
zhimin8037 <xibar8037@gmail.com>
2023-11-10 01:29:40 +0900

    add counter

--- main.js
+++
@@ -1,8 +0,0 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getConrext('2d');
-
-canvas.width = window.innerWidth - 100;
-canvas.height = window.innerHeight - 100;
-
-ctx.fillstyle = 'green';
-ctx.fillrect(10,10, 100,100);
